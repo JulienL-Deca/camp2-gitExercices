97ca45c9c8dd905fda660b900e86d7786d09f465
JULIEN LI <31632821+JulienL-Deca@users.noreply.github.com>
2018-04-05 16:54:30 +0200

    example of git command listed in a file

---
+++ git_commands.sh
@@ -0,0 +1,16 @@
+# Initialisation d’un répertoire pour git
+# $ git init
+
+# Changer de repository
+# $ git checkout name
+
+# Obtenir info d’un repository git, à faire en fin de journée et après chaque commit pour s’assurer que tout est clean
+# $ git status
+
+# Voir les changements effectué dans une branche
+# $ git log
+
+# Voir le détail des changements sous forme de diff
+# $ git log -p // voir toutes les modifs
+# $ git show ID_COMMIT // voir uniquement la modif ID_COMMIT
+# $ git diff ID_COMMIT
